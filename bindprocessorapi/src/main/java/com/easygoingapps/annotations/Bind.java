02395d31a940c90c12d5b645d498c4a0c299393b
Marcus Hooper <hooperarma@gmail.com>
2015-10-13 20:53:40 +1000

    Initial Commit

---
+++ bindprocessorapi/src/main/java/com/easygoingapps/annotations/Bind.java
@@ -0,0 +1,14 @@
+package com.easygoingapps.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.FIELD)
+public @interface Bind
+{
+	//ResId of view to bind to annotated field
+	int value();
+}
---
+++ bindprocessorcompiler/src/main/java/com/easygoingapps/BindProcessor.java
@@ -0,0 +1,26 @@
+package com.easygoingapps;
+
+import com.easygoingapps.annotations.Bind;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import java.util.Set;
+
+@SupportedAnnotationTypes("com.easygoingapps.annotations.Bind")
+@SupportedSourceVersion(SourceVersion.RELEASE_7)
+public class BindProcessor extends AbstractProcessor
+{
+	@Override
+	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv)
+	{
+		for(Element element : roundEnv.getElementsAnnotatedWith(Bind.class))
+			processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "Actually processing something", element);
+		return true;
+	}
+}
