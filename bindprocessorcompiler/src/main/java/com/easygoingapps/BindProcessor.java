9193180700c3870269a162416ea3ec81da0fc0bd
Marcus Hooper <hooperarma@gmail.com>
2015-10-14 08:20:15 +1000

    Generating very specific custom file

--- bindprocessorcompiler/src/main/java/com/easygoingapps/BindProcessor.java
+++ bindprocessorcompiler/src/main/java/com/easygoingapps/BindProcessor.java
@@ -45,6 +45,7 @@ public class BindProcessor extends AbstractProcessor
 				PackageElement packageElement = (PackageElement) classElement.getEnclosingElement();
 				try
 				{
+					String className = classElement.getSimpleName() + "ViewBinding";
 					JavaFileObject jfo = filer.createSourceFile(classElement.getQualifiedName() + "ViewBinding");
 					BufferedWriter writer = new BufferedWriter(jfo.openWriter());
 					writer.append("package ");
@@ -52,6 +53,31 @@ public class BindProcessor extends AbstractProcessor
 					writer.append(";");
 					writer.newLine();
 					writer.newLine();
+					writer.append("import android.app.Activity;");
+					writer.newLine();
+					writer.append("import android.widget.EditText;");
+					writer.newLine();
+					writer.newLine();
+					writer.append("public class ").append(className);
+					writer.newLine();
+					writer.append("{");
+					writer.newLine();
+					//Start of code
+
+					//Start of constructor
+					writer.append("    public ").append(className).append(" (Activity activity)");
+					writer.newLine();
+					writer.append("    {");
+					writer.newLine();
+					writer.append("        EditText editText = (EditText) activity.findViewById(")
+							.append("" + element.getAnnotation(Bind.class).value())
+							.append(");");
+					writer.newLine();
+					writer.append("    }");
+					writer.newLine();
+					//End of constructor
+
+					writer.append("}");
 					writer.close();
 				}
 				catch(IOException e)
